5862e44ad459a2cf74780eac9646f3a02332c57c
salmanseptianto <salmanseptianto27@gmail.com>
2023-11-13 20:51:54 +0700

    add uji coba pertama/test.ts

--- test.ts
+++ test.ts
@@ -1,25 +1,30 @@
 /** @format */
 
 const conversiNilai = (a: number) => {
-    switch (a) {
-      case 90:
-        console.log("Nilai adalah 90");
-        break;
-      case 80:
-        console.log("Nilai adalah 80");
-        break;
-      default:
-        break;
-    }
-  };
-  
-//   conversiNilai(80);
-  
+  switch (a) {
+    case 90:
+      console.log("Nilai adalah 90");
+      break;
+    case 80:
+      console.log("Nilai adalah 80");
+      break;
+    default:
+      break;
+  }
+};
 
+//   conversiNilai(80);
 
 const treeName = (name: string) => {
   for (let i = 0; i < name.length; i++) {
     console.log(name[i]);
   }
 };
-console.log(treeName("salman"));
+
+const factorial = (n: number): number => {
+  if (n == 1) {
+    return n;
+  } else return n * factorial(n - 1);
+};
+factorial(80);
+// console.log(treeName("salman"));
